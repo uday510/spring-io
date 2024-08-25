b88ac9ec57a2264454f44e78d316b589e0ce4347
Uday <budayteja009@gmail.com>
2024-08-25 16:19:32 +0530

    Add IOC

---
+++ src/main/java/com/app/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.app.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        Alien alien = context.getBean(Alien.class);
+        alien.code();
+
+    }
+}
