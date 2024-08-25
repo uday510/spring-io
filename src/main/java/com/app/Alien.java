742b1fe057649013f4d11f40630422f13b3dd2a6
Uday <budayteja009@gmail.com>
2024-08-25 17:49:37 +0530

    Add Constructor Injection

--- src/main/java/com/app/Alien.java
+++ src/main/java/com/app/Alien.java
@@ -8,6 +8,17 @@ public class Alien {
         System.out.println("Alien object created" );
     }
 
+    public Alien(int age) {
+        this.age = age;
+        System.out.println("Alien object created with age: " + age);
+    }
+
+    public Alien(int age, Laptop laptop) {
+        this.age = age;
+        this.laptop = laptop;
+        System.out.println("Alien object created with age: " + age + " and laptop: " + laptop);
+    }
+
     public void code() {
         System.out.println("Alien is coding");
         laptop.compile();
